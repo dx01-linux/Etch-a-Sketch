0a6b50994b0c5f43d8a4368081e9e86e19e91847
Dx <dxcode01@gmail.com>
2024-01-31 08:48:34 -0600

    - Mouse hover give a random color to any square passed over

--- scripts/scrip1.js
+++ scripts/scrip1.js
@@ -1,14 +1,6 @@
 
 const attachRows = document.querySelector('#attach-rows');
 const colors = document.querySelectorAll('.colors');
-const selectedColor = {
-    tag : document.querySelector('#selected-color') ,
-    value : 'bisque' ,
-    setBackgroundColor : function (color) {
-        this.value = color ;
-        this.tag.style.backgroundColor = this.value ;
-    }
-}
 const squares = {
     tags : function () {
         return document.querySelectorAll('.square');
@@ -16,28 +8,13 @@ const squares = {
 }
 setUpTable(16, 16);
 
-// change selectedColor's background color and and this.value's value
-colors.forEach(color=> {
-    color.addEventListener('click' , e=> {
-        let backgroundColors= ['bisque' , 'black' , 'red'];
-        switch (color.innerText ){
-            case 'white' :
-                selectedColor.setBackgroundColor(backgroundColors[0]);
-                break ;
-            case 'black' :
-                selectedColor.setBackgroundColor(backgroundColors[1]);
-                break;
-            case 'red' :
-                selectedColor.setBackgroundColor(backgroundColors[2]);
-                break;
-        }
-    })
-})
-
 //change color of a single square using actual selectedColor.value's value
 squares.tags().forEach(tag => {
-    tag.addEventListener('click' , e=>{
-        tag.style.backgroundColor = selectedColor.value ;
+    tag.addEventListener('mousemove' , e=>{
+        let r = getNumber(256);
+        let g = getNumber(256);
+        let b = getNumber(256);
+        tag.style.backgroundColor = `rgb(${r} , ${g} , ${b})`;
     })
 })
 
@@ -60,3 +37,7 @@ function setUpTable(rowsNumber= 16 , elementsPerRow = 16){
         attachRows.appendChild(row);
     });
 }    
+
+function getNumber(max){
+    return Math.floor(Math.random() * max)
+}
